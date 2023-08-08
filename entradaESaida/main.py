9aafa30b90fce228573dd40003a3dffaa2126b47
Felipe Felix <felipe.dfelix@gmail.com>
2023-08-08 16:09:54 -0300

    feat: Entrada e Saida em python

---
+++ entradaESaida/main.py
@@ -0,0 +1,23 @@
+print("Hello World!!")
+
+idade = 20
+salario = 5800.5
+altura = 1.72
+genero = "M"
+nome = "Felipe"
+sobreNome = "Felix"
+
+
+
+# Interpolacao + formatacao
+print(f"Idade: {idade}")
+print(f"Salario: {salario:.2f}")
+print(f"Altura: {altura:.2f}")
+print(f"Genero: {genero}")
+print(f"Nome Completo: {nome} {sobreNome}")
+
+#Entrada de dados
+endereco = str(input("Informe o endereco: "))
+estado = str(input("Informe o Estado: "))
+
+print(f"Endereco: {endereco} {estado}")
