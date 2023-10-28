b7d684f943c71c899bc1ad86b28e8f82d34100b7
Felipe Felix <felipe.dfelix@gmail.com>
2023-10-28 17:05:53 -0300

    Formas de criar uma lista

---
+++ estruturasDeDados/main.py
@@ -0,0 +1,17 @@
+# Listas
+
+frutas = ["laranja", "manga", "melancia"]
+
+verduras = []
+
+letras = list("python")
+
+numeros = list(range(10))
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "Sao Paulo", True]
+
+print(frutas)
+print(verduras)
+print(letras)
+print(numeros)
+print(carro)
