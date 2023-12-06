f410af84453219372bb40058576a02ac9457e04f
Felipe Felix <felipe.dfelix@gmail.com>
2023-12-06 17:37:49 -0300

    Encapsulamento com atributo "privado"

---
+++ Poo/Encapsulamento/encapsulamento.py
@@ -0,0 +1,15 @@
+class Conta:
+    def __init__(self, saldo=0):
+        self._saldo = saldo
+
+    def depositar(self, valor):
+        self._saldo += valor
+
+    def sacar(self, valor):
+        self._saldo -= valor
+
+conta = Conta(100)
+conta.depositar(200)
+conta.sacar(50)
+
+print(conta._saldo)
